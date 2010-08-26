3909fd7012921430679d8c5224bc6253572fd70f
Graham Hughes <graham@rightscale.com>
2010-08-26 16:38:04 -0700

    Permit running the spec file repeatedly with no warnings.

--- spec/right_popen_spec.rb
+++ spec/right_popen_spec.rb
@@ -1,21 +1,5 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
 
-RUBY_CMD         = 'ruby'
-STANDARD_MESSAGE = 'Standard message'
-ERROR_MESSAGE    = 'Error message'
-EXIT_STATUS      = 146
-
-# manually bump count up for more aggressive multi-processor testing, lessen
-# for a quick smoke test
-LARGE_OUTPUT_COUNTER = 1000
-
-# bump up count for most exhaustive leak detection.
-REPEAT_TEST_COUNTER = 256
-
-def is_windows?
-  return RUBY_PLATFORM =~ /mswin/
-end
-
 describe 'RightScale::popen3' do
 
   module RightPopenSpec
